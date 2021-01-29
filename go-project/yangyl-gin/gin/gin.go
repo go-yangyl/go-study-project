368e249640f5b6c31df5dcf03308ab9c79d9a13f
yang-yi-li <2677808632@qq.com>
2021-01-29 16:29:49 +0800

    group

--- go-project/yangyl-gin/gin/gin.go
+++ go-project/yangyl-gin/gin/gin.go
@@ -7,35 +7,51 @@ import (
 
 type HandlerFunc func(*Context)
 
+type RouterGroup struct {
+	prefix string
+	engine *Engine // all groups share a Engine instance
+}
+
 type Engine struct {
 	router *router
+	*RouterGroup
 }
 
 func New() *Engine {
-	return &Engine{router: NewRouter()}
+	engine := &Engine{router: NewRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	return engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	newRouterGroup := new(RouterGroup)
+	newRouterGroup.prefix = prefix
+	newRouterGroup.engine = group.engine
+	return newRouterGroup
 }
 
 // 添加路由 method+"-"+pattern
-func (e *Engine) AddRouter(method, pattern string, handler HandlerFunc) {
-	log.Printf("method:%s,pattern:%s", method, pattern)
-	e.router.AddRouter(method, pattern, handler)
+func (group *RouterGroup) AddRouter(method, pattern string, handler HandlerFunc) {
+	path := group.prefix + pattern
+	log.Printf("method:%s,pattern:%s", method, path)
+	group.engine.router.AddRouter(method, path, handler)
 }
 
-func (e *Engine) GET(pattern string, handler HandlerFunc) {
-	e.AddRouter("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.AddRouter("GET", pattern, handler)
 }
 
-func (e *Engine) POST(pattern string, handler HandlerFunc) {
-	e.AddRouter("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.AddRouter("POST", pattern, handler)
 }
 
-func (e *Engine) PUT(pattern string, handler HandlerFunc) {
-	e.AddRouter("PUT", pattern, handler)
+func (group *RouterGroup) PUT(pattern string, handler HandlerFunc) {
+	group.AddRouter("PUT", pattern, handler)
 }
 
-func (e *Engine) Run(addr string) error {
+func (group *RouterGroup) Run(addr string) error {
 	log.Printf("server is running %s", addr)
-	return http.ListenAndServe(addr, e)
+	return http.ListenAndServe(addr, group.engine)
 }
 
 // 实现http.Handler接口
